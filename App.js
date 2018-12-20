04820569991f7ddae46763f66b0181c573238495
LindaKadz <achielinda44@gmail.com>
2018-12-20 12:28:13 +0300

    show login form

--- App.js
+++ App.js
@@ -7,26 +7,33 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, AppRegistry, ScrollView, TextInput, Button} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+
+// const instructions = Platform.select({
+//   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+//   android:
+//     'Double tap R on your keyboard to reload,\n' +
+//     'Shake or press menu button for dev menu',
+// });
+
 
 type Props = {};
 export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
+  constructor(props) {
+    super(props);
+    this.state = { email: ' ', password: ' '}
   }
+  render() {
+   return (
+     <View style={styles.container}>
+       <Text style={styles.welcome}> Login </Text>
+       <TextInput placeholder= 'Email' />
+       <TextInput placeholder= 'Password' />
+       <Button title='Login' backgroundColor='#FFFD700'/>
+     </View>
+   );
+}
 }
 
 const styles = StyleSheet.create({
@@ -40,10 +47,5 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+   },
 });
